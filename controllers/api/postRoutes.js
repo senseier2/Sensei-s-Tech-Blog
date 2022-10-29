616e2d0ed3f70d4907005aaf5026d2d5d00f697d
Senfire <eric.mcghee@northwestern.edu>
2022-10-29 01:05:55 -0500

    adding notes for TODO's

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,17 @@
+// Posting requires authentication
+
+//Create a post - should belong to one user
+
+
+//Delete a post - from a user
+
+// Get all comments
+
+
+// find one comment
+
+
+// Create a comment
+
+// Delete a comment
+
---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,11 @@
+//Create a new User - add a user to the database
+
+
+
+//User login - create a logged-in session
+
+
+
+
+//User logout - end the session
+
