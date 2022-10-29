e8a6b0ee5ac12a873f4b5c8e0eec0fd413c057bc
Senfire <eric.mcghee@northwestern.edu>
2022-10-29 00:53:53 -0500

    Starting the routing for the controllers prior to route creation - pending route tests with get all routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,11 @@
-//setup index to point 
+//setup index to point to express routes home and api routes
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
+
